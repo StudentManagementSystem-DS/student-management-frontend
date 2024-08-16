66846ed537aaa6d833fd972628519aeeb88bca07
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-16 18:00:59 +0300

    add: StudentsView component for fetching student data by the backend endpoint and displaying the students

---
+++ src/component/student/StudentView.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+
+const StudentsView = () => {
+
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+        loadStudents();
+    },[])
+
+    const loadStudents = async () =>
+    {
+        const result = await axios.get(
+            'http://localhost:9192/students'
+        );
+        setStudents(result.data)
+    }
+
+    return(
+        <section>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Email</th>
+                        <th>Department</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {students.map((student, index) => (
+                    <tr key={student.id}>
+                        <th scope="row" key={index}>
+                            {index + 1}
+                        </th>
+                    <td>{student.firstName}</td>
+                    <td>{student.lastName}</td>
+                    <td>{student.email}</td>
+                    <td>{student.department}</td>
+                    <td>View</td>
+                    <td>Update</td>
+                    <td>Delete</td>
+                </tr>
+                    ))}
+                </tbody>
+            </table>
+
+        </section>
+    )
+}
+
+export default StudentsView
