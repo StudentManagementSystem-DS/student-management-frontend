c9e3a476565390fcd3ea7d4ba2ee71d33fc36a23
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-17 21:10:13 +0300

    add: implement routing in App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 import './App.css';
 import StudentsView from './component/student/StudentView';
+import NavBar from "./component/common/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <StudentsView/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar/>
+          <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/view-students" element={<StudentsView/>}></Route>
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
