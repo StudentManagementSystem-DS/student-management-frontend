8d446a259635438f790648fcb54f932d31a96ecd
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-18 17:24:21 +0300

    add: table shadow style and icons for the update and view buttons

--- src/component/student/StudentView.js
+++ src/component/student/StudentView.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import { FaEye, FaTrashAlt, FaEdit } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const StudentsView = () => {
     const [students, setStudents] = useState([]);
@@ -21,7 +23,7 @@ const StudentsView = () => {
 
     return (
         <section>
-            <table className="table table-bordered table-hover">
+            <table className="table table-bordered table-hover shadow">
                 <thead>
                     <tr className="text-center">
                         <th>ID</th>
@@ -41,13 +43,21 @@ const StudentsView = () => {
                             <td>{student.email}</td>
                             <td>{student.department}</td>
                             <td className="mx-2">
-                                <button className="btn btn-info">View</button>
+                                <Link 
+                                to={`/student-profile/${student.id}`}
+                                className="btn btn-info">
+                                <FaEye />
+                                </Link>
                             </td>
                             <td className="mx-2">
-                                <button className="btn btn-warning">Update</button>
+                                <Link 
+                                to={`/edit-student/${student.id}`}
+                                className="btn btn-warning">
+                                <FaEdit />
+                                </Link>
                             </td>
                             <td className="mx-2">
-                                <button className="btn btn-danger">Delete</button>
+                                <button className="btn btn-danger"><FaTrashAlt/></button>
                             </td>
                         </tr>
                     ))}
