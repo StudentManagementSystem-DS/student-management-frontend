4cdb5151a11175e783f88c62a8e1c11db6b664b9
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-18 16:55:01 +0300

    add: apply container and margin-top styles for App component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ import Home from "./Home";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar/>
-          <Routes>
-            <Route exact path="/" element={<Home/>}></Route>
-            <Route exact path="/view-students" element={<StudentsView/>}></Route>
-          </Routes>
-      </div>
-    </Router>
+    <main className="container mt-5">
+      <Router>
+        <div className="App">
+          <NavBar/>
+            <Routes>
+              <Route exact path="/" element={<Home/>}></Route>
+              <Route exact path="/view-students" element={<StudentsView/>}></Route>
+            </Routes>
+        </div>
+      </Router>
+    </main>
   );
 }
 
