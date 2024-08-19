18a0f80b1317f767b54c3ece84e4f6aeb97ce7ab
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-19 18:28:16 +0300

    add: prettier format for single quotes only used in js/jsx files and double quotes used only in HTML files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 import StudentsView from './component/student/StudentView';
-import NavBar from "./component/common/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import AddStudent from "./component/student/AddStudent";
-import EditStudent from "./component/student/EditStudent";
-import StudentProfile from "./component/student/StudnetProfile";
+import NavBar from './component/common/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import AddStudent from './component/student/AddStudent';
+import EditStudent from './component/student/EditStudent';
+import StudentProfile from './component/student/StudnetProfile';
 
 function App() {
   return (
     <main className="container mt-5">
       <Router>
         <div className="App">
-          <NavBar/>
-            <Routes>
-              <Route exact path="/" element={<Home/>}></Route>
-              <Route exact path="/view-students" element={<StudentsView/>}></Route>
-              <Route exact path="/add-students" element={<AddStudent/>}></Route>
-              <Route exact path="/edit-student/:id" element={<EditStudent/>}></Route>
-              <Route exact path="/student-profile/:id" element={<StudentProfile/>}></Route>
-            </Routes>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route
+              exact
+              path="/view-students"
+              element={<StudentsView />}
+            ></Route>
+            <Route exact path="/add-students" element={<AddStudent />}></Route>
+            <Route
+              exact
+              path="/edit-student/:id"
+              element={<EditStudent />}
+            ></Route>
+            <Route
+              exact
+              path="/student-profile/:id"
+              element={<StudentProfile />}
+            ></Route>
+          </Routes>
         </div>
       </Router>
     </main>
